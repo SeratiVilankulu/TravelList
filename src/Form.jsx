c5aa4efa6d33a7f538529ffdf06686d609250ffc
SeratiVilankulu <seratimotla@gmail.com>
2026-06-17 15:28:49 +0200

    Building a Form and Handling Submissions

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,26 @@
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
     <>
-      <div className="add-form">
+      <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for you trip?</h3>
-      </div>
+        <select>
+          <option value="" disabled>
+            Choose an item
+          </option>
+          {Array.from({ length: 15 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input type="text" placeholder="Item..." />
+        <button>Add</button>
+      </form>
     </>
   );
 }
