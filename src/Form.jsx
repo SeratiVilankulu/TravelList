2825f00e2ca79d028e0c0222da471a4db0877e0d
SeratiVilankulu <seratimotla@gmail.com>
2026-06-18 14:34:41 +0200

    Controlled Elements

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <>
       <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for you trip?</h3>
-        <select>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
           <option value="" disabled>
             Choose an item
           </option>
@@ -18,7 +33,12 @@ function Form() {
             </option>
           ))}
         </select>
-        <input type="text" placeholder="Item..." />
+        <input
+          type="text"
+          placeholder="Item..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button>Add</button>
       </form>
     </>
