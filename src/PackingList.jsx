dcb4904712984c6045973ee6f5d553d94641563f
SeratiVilankulu <seratimotla@gmail.com>
2026-07-29 09:56:26 +0200

    Toggle items

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,12 +1,12 @@
 import Item from "./Item";
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <>
       <div className="list">
         <ul>
           {items.map((item) => (
-            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
           ))}
         </ul>
       </div>
