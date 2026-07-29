02e29bc4721984a058e384d96366532c8ef681a7
SeratiVilankulu <seratimotla@gmail.com>
2026-07-29 10:21:38 +0200

    Adding stats for items

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,8 +1,23 @@
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <em className="stats">Start adding some items to your packing list 📝</em>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <>
       <footer className="stats">
-        <em>🎒 You have X items on you list, and yuou already packed X (X%)</em>
+        <em>
+          {percentage === 100
+            ? "You got everything! Ready to go ✈️"
+            : `🎒 You have ${numItems} items on you list, and yuou already packed
+          ${numPacked}(${percentage}%)
+`}
+        </em>
       </footer>
     </>
   );
